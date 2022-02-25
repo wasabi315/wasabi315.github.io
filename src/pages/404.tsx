a6db818ae38a049fd5a1fa76de14914a5b7913c9
wasabi315 <wasabi.nvim.st@gmail.com>
2022-02-25 11:23:02 +0900

    Adjust 404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,15 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "./404.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className={styles.title}>404: Not Found</h1>
+    <p className={styles.content}>
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </p>
   </Layout>
 )
 
