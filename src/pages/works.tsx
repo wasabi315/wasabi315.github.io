35ba2740d1faf75877f0e97adf6428233eba18a5
wasabi315 <wasabi.nvim.st@gmail.com>
2022-02-26 21:55:37 +0900

    Under construction

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,48 +1,23 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHelmetSafety } from "@fortawesome/free-solid-svg-icons"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "./works.module.scss"
 
-type Prop = {
-  data: {
-    allMdx: {
-      nodes: {
-        slug: string
-        frontmatter: {
-          title: string
-        }
-      }[]
-    }
-  }
-}
-
-const WorksPage: React.FCX<Prop> = ({ data }) => (
+const WorksPage = () => (
   <Layout>
     <Seo title="Works" />
     <h1 className={styles.title}>Works</h1>
-    <ul>
-      {data.allMdx.nodes.map(({ slug, frontmatter }) => (
-        <li key={slug}>
-          <Link to={`/${slug}`}>{frontmatter.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <p className={styles.body}>
+      <FontAwesomeIcon icon={faHelmetSafety} />
+      {` `}
+      Under Construction
+      {` `}
+      <FontAwesomeIcon icon={faHelmetSafety} />
+    </p>
   </Layout>
 )
 
 export default WorksPage
-
-export const pageQuery = graphql`
-  query {
-    allMdx(filter: { slug: { regex: "/^works//" } }) {
-      nodes {
-        slug
-        frontmatter {
-          title
-        }
-      }
-    }
-  }
-`
