5b5ee64dc0fef2c7e4bde4ace4a37a0c0b841c02
wasabi315 <wasabi.nvim.st@gmail.com>
2022-03-07 14:30:02 +0900

    Shadow on scroll

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,38 +16,56 @@ const navigationLinks = [
   { to: `/posts`, label: `Posts` },
 ]
 
+const useScrollY = (): number => {
+  const [scrollY, setScrollY] = React.useState(0)
+  React.useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY)
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+  return scrollY
+}
+
 const Header: React.FCX<Prop> = ({
   className,
   siteTitle = ``,
   onClickMenu,
-}) => (
-  <header className={`${className} ${styles.header}`}>
-    <nav className={styles.container}>
-      <div className={styles.title}>
-        <Link to="/">{siteTitle}</Link>
-      </div>
-      <ul className={styles.nav}>
-        {navigationLinks.map(({ to, label }) => (
-          <li key={to} className={styles.nav_item}>
-            <Link to={to}>{label}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.spacer} />
-      <ul className={styles.nav}>
-        {config.socialLinks.map(({ icon, url }) => (
-          <li key={url} className={styles.nav_item}>
-            <a href={url}>
-              <FontAwesomeIcon icon={icon} />
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.menu}>
-        <FontAwesomeIcon icon={faBars} onClick={onClickMenu} />
-      </div>
-    </nav>
-  </header>
-)
+}) => {
+  const scrollY = useScrollY()
+  const showHeaderShadow = scrollY > 0
+  return (
+    <header
+      className={`${className} ${styles.header} ${
+        showHeaderShadow ? styles.header_shadow : ``
+      }`}
+    >
+      <nav className={styles.container}>
+        <div className={styles.title}>
+          <Link to="/">{siteTitle}</Link>
+        </div>
+        <ul className={styles.nav}>
+          {navigationLinks.map(({ to, label }) => (
+            <li key={to} className={styles.nav_item}>
+              <Link to={to}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.spacer} />
+        <ul className={styles.nav}>
+          {config.socialLinks.map(({ icon, url }) => (
+            <li key={url} className={styles.nav_item}>
+              <a href={url}>
+                <FontAwesomeIcon icon={icon} />
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.menu}>
+          <FontAwesomeIcon icon={faBars} onClick={onClickMenu} />
+        </div>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
