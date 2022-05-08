d2ed7d5e3d312f41c252f4e34f00340ac6c5a1b7
wasabi315 <wasabi.nvim.st@gmail.com>
2022-05-08 13:28:46 +0900

    Update work template style

---
+++ src/templates/work/article.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+
+import MDXRenderer from "../../components/mdx-renderer";
+import ContentGitHubLink from "../../components/content-github-link";
+import * as styles from "./article.module.scss";
+
+type Prop = {
+  work: {
+    body: string;
+    fields: {
+      filePath: string;
+    };
+    frontmatter: {
+      title: string;
+      description: string;
+      githubRepository?: string;
+      featuredImage: ImageDataLike;
+    };
+  };
+};
+
+const Article: React.FCX<Prop> = ({ work }) => {
+  const featuredImage = getImage(work.frontmatter.featuredImage);
+  return (
+    <article className={styles.article}>
+      <header>
+        <div>
+          <h1>{work.frontmatter.title}</h1>
+          {work.frontmatter.githubRepository && (
+            <a href={`https://github.com/${work.frontmatter.githubRepository}`}>
+              <FontAwesomeIcon icon={faGithubAlt} />
+              {` `}
+              View on GitHub
+            </a>
+          )}
+        </div>
+        <p>{work.frontmatter.description}</p>
+      </header>
+      <figure>
+        {featuredImage && (
+          <GatsbyImage
+            className={styles.featured_image}
+            image={featuredImage}
+            alt="featured image"
+          />
+        )}
+      </figure>
+      <MDXRenderer frontmatter={work.frontmatter}>{work.body}</MDXRenderer>
+      <footer>
+        <FontAwesomeIcon icon={faGithubAlt} />
+        {` `}
+        <ContentGitHubLink filePath={work.fields.filePath}>
+          View this article on GitHub
+        </ContentGitHubLink>
+      </footer>
+    </article>
+  );
+};
+
+export default Article;
--- src/templates/work/index.tsx
+++ src/templates/work/index.tsx
@@ -1,14 +1,10 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubAlt, faGitlab } from "@fortawesome/free-brands-svg-icons";
+import { ImageDataLike } from "gatsby-plugin-image";
 
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
-import MDXRenderer from "../../components/mdx-renderer";
-import ContentGitHubLink from "../../components/content-github-link";
-import * as styles from "./index.module.scss";
+import Article from "./article";
 
 type Prop = {
   data: {
@@ -28,46 +24,10 @@ type Prop = {
 };
 
 const Work: React.FCX<Prop> = ({ data: { mdx } }) => {
-  const featuredImage = getImage(mdx.frontmatter.featuredImage);
   return (
     <Layout>
       <Seo title={mdx.frontmatter.title} />
-      <article>
-        <header className={styles.header}>
-          <div className={styles.title_container}>
-            <h1 className={styles.title}>{mdx.frontmatter.title}</h1>
-            {mdx.frontmatter.githubRepository && (
-              <a
-                className={styles.repo_url}
-                href={`https://github.com/${mdx.frontmatter.githubRepository}`}
-              >
-                <FontAwesomeIcon className={styles.icon} icon={faGithubAlt} />
-                View on GitHub
-              </a>
-            )}
-          </div>
-          <p className={styles.description}>{mdx.frontmatter.description}</p>
-        </header>
-        <div className={styles.featured_image_wrapper}>
-          {featuredImage && (
-            <GatsbyImage
-              className={styles.featured_image}
-              image={featuredImage}
-              alt="featured image"
-            />
-          )}
-        </div>
-        <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
-        <footer className={styles.footer}>
-          <ContentGitHubLink
-            className={styles.link}
-            filePath={mdx.fields.filePath}
-          >
-            <FontAwesomeIcon className={styles.icon} icon={faGithubAlt} />
-            View this article on GitHub
-          </ContentGitHubLink>
-        </footer>
-      </article>
+      <Article work={mdx} />
     </Layout>
   );
 };
