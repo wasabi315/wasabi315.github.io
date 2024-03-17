0571248cdfa70b9575499f18b367bb0e055c03b9
wasabi315 <wasabi.nvim.st@gmail.com>
2024-03-17 21:44:00 +0900

    Update index

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    description: z.string(),
     date: z.string(),
     tags: z.array(z.string()),
   }),
--- src/content/posts/hello.md
+++ src/content/posts/hello.md
@@ -1,5 +1,6 @@
 ---
 title: "Hello!"
+description: "This is the first post of my blog."
 date: "2022-02-20"
 tags: ["random"]
 ---
