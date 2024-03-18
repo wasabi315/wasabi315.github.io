71f850e7977c8d454c80b5a4b68d78ff556c2ea3
wasabi315 <wasabi.nvim.st@gmail.com>
2024-03-18 11:15:40 +0900

    Add katex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeKatex from "rehype-katex";
 import icon from "astro-icon";
 import agda from "./src/data/agda.tmLanguage.json";
 
@@ -15,10 +17,11 @@ export default defineConfig({
       theme: "nord",
       // langs: [agda],
     },
-    remarkPlugins: [remarkToc],
+    remarkPlugins: [remarkToc, remarkMath],
     rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: "append" }],
+      rehypeKatex,
     ],
   },
 });
