cb7042cedfb3937232a78ce4cfd09d663613d3d4
wasabi315 <wasabi.nvim.st@gmail.com>
2024-12-15 12:28:41 +0900

    Migrate content collections API

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,11 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const postsCollection = defineCollection({
-  type: "content",
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "src/content/posts",
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -11,7 +15,10 @@ const postsCollection = defineCollection({
 });
 
 const worksCollection = defineCollection({
-  type: "content",
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "src/content/works",
+  }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
