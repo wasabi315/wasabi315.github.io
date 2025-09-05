329886bdf189a06995f413cba23c6157fc85c56c
wasabi315 <wasabi.nvim.st@gmail.com>
2025-09-05 17:17:41 +0900

    Use Array.fromAsync

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,5 @@
 import type { CollectionEntry } from "astro:content";
 
-// Array.fromAsync cannot be used for getStaticPaths
-export async function arrayFromAsyncIterable<T>(
-  asyncIterable: AsyncIterable<T>
-): Promise<Array<T>> {
-  const result = [];
-  for await (const elem of asyncIterable) {
-    result.push(elem);
-  }
-  return result;
-}
-
 export const sortPosts = (
   posts: CollectionEntry<"posts">[]
 ): CollectionEntry<"posts">[] =>
